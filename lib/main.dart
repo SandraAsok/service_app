66013d5181d757edbc9ff429ecc8917e3b8856b7
SandraAsok <116081047+SandraAsok@users.noreply.github.com>
2024-03-07 09:31:40 +0530

    firebase commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,17 @@ void main() {
   ));
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
 
