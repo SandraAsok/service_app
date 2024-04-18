8624b8744cd3e07dadfcc568c918b53f7a0e923b
SandraAsok <116081047+SandraAsok@users.noreply.github.com>
2024-04-18 13:16:49 +0530

    url launcher commit

--- lib/screens/services/labour_list.dart
+++ lib/screens/services/labour_list.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:service_app/screens/services/labour_detail.dart';
 import 'package:service_app/utilities/utilities.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LabourList extends StatefulWidget {
   final String job;
@@ -21,88 +22,110 @@ class _LabourListState extends State<LabourList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              space,
-              SizedBox(
-                height: 900,
-                child: StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('labours')
-                        .where('job', isEqualTo: widget.job)
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            final document = snapshot.data!.docs[index];
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      CupertinoPageRoute(
-                                          builder: (context) => LabourDetail(
-                                                name: document['name'],
-                                                image: document['image'][0],
-                                                age: document['age'],
-                                                details: document['details'],
-                                                address: document['address'],
-                                              ),
-                                          fullscreenDialog: true));
-                                },
-                                child: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        gradient: LinearGradient(
-                                            colors: [theme_color, Colors.white],
-                                            begin: Alignment.topLeft,
-                                            end: Alignment.bottomRight)),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Padding(
-                                          padding: EdgeInsets.all(20.0),
-                                          child: Text(
-                                            document['name'],
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ),
-                                        IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(Icons.phone)),
-                                        ElevatedButton.icon(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                                Icons.calendar_month_outlined),
-                                            label: const Text("Book")),
-                                        CircleAvatar(
-                                          backgroundColor: Colors.purple,
-                                          radius: 35,
-                                          backgroundImage: NetworkImage(
-                                              document['image'][0]),
-                                        )
-                                      ],
-                                    )),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            space,
+            SizedBox(
+              height: 900,
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('labours')
+                    .where('job', isEqualTo: widget.job)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final document = snapshot.data!.docs[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => LabourDetail(
+                                    name: document['name'],
+                                    image: document['image'][0],
+                                    age: document['age'],
+                                    details: document['details'],
+                                    address: document['address'],
+                                  ),
+                                  fullscreenDialog: true,
+                                ),
+                              );
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  colors: [theme_color, Colors.white],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
                               ),
-                            );
-                          },
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: Text(
+                                      document['name'],
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      _launchPhoneCall(document[
+                                          'phone']); // Call function to launch phone call
+                                    },
+                                    icon: Icon(Icons.phone),
+                                  ),
+                                  ElevatedButton.icon(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.calendar_month_outlined),
+                                    label: Text("Book"),
+                                  ),
+                                  CircleAvatar(
+                                    backgroundColor: Colors.purple,
+                                    radius: 35,
+                                    backgroundImage: NetworkImage(
+                                      document['image'][0],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         );
-                      } else if (snapshot.hasError) {
-                        log("ERROR While Loading labours ::: ${snapshot.error}");
-                      }
-                      return CircularProgressIndicator();
-                    }),
-              )
-            ],
-          ),
-        ));
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    log("ERROR While Loading labours ::: ${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _launchPhoneCall(String phoneNumber) async {
+    String telScheme = 'tel:$phoneNumber';
+    if (await canLaunchUrl(telScheme as Uri)) {
+      await launchUrl(telScheme as Uri);
+    } else {
+      throw 'Could not launch $telScheme';
+    }
   }
 }
